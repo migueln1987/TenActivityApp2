d937d6d36e7c954fb5d298f16f4a0863781208c3
Miguel Najera <migueln1987@gmail.com>
2019-03-15 03:43:14 -0400

    Set every other activity to portrait mode only

--- app/src/main/java/com/example/tenactivityapp/activities/ActivityTen.kt
+++ app/src/main/java/com/example/tenactivityapp/activities/ActivityTen.kt
@@ -2,6 +2,7 @@ package com.example.tenactivityapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import com.example.tenactivityapp.R
 
@@ -10,5 +11,6 @@ class ActivityTen : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ten)
+        Toast.makeText(this, "You've made it to the end", Toast.LENGTH_SHORT).show()
     }
 }
