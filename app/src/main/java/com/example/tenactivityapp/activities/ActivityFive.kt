6b90445614a98d7427bbb906b2c3234748f9b94a
Miguel Najera <migueln1987@gmail.com>
2019-03-15 12:58:45 -0400

    Add Olay's snackbar

--- app/src/main/java/com/example/tenactivityapp/activities/ActivityFive.kt
+++ app/src/main/java/com/example/tenactivityapp/activities/ActivityFive.kt
@@ -1,11 +1,17 @@
 package com.example.tenactivityapp.activities
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.tenactivityapp.R
 import kotlinx.android.synthetic.main.activity_five.*
 
@@ -13,24 +19,40 @@ class ActivityFive : AppCompatActivity() {
 
     val CHANNEL_ID = "halfway"
     val TAG = "ActivityFive"
+    lateinit var builder :NotificationCompat.Builder
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_five)
         Log.d(TAG, "onCreate")
-        Toast.makeText(this, "You're halfway there", Toast.LENGTH_SHORT).show()
-        button_to_six.setOnClickListener{
-            startActivity(Intent(this, ActivitySix::class.java))
+        builder = notificationStuff()
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(createNotificationChannel())
         }
+        notificationManager.notify(1, builder.build())
     }
 
-    fun notificationStuff () {
+    fun notificationStuff (): NotificationCompat.Builder {
 
-        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+        return NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.notification_template_icon_bg)
             .setContentTitle("Halfway Notification")
             .setContentText("You're halfway there")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel() : NotificationChannel {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            return NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+        }
+    }
+
 
     override fun onStart() {
         super.onStart()
